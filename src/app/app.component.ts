8ae10ce83818605b809782fd8e6b338dd117d1c3
Yorlin Quispe <80475432+KDONJS@users.noreply.github.com>
2024-04-29 20:29:29 -0500

    inicio de routing - proyecto: AS-PROJECT-FRONT-01924

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/helpers/navbar/navbar.component";
+import { FooterComponent } from "./components/helpers/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, NavbarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'AS-PROJECT-FRONT-01924';
